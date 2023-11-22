fe7f62fc2db632c1c4462fe93b779f49ad2ab1f4
ilhamelhamdi <ilhamabdillah123@gmail.com>
2023-11-22 20:29:58 +0700

    feat: logout integration with webservice

--- lib/presentation/pages/home.dart
+++ lib/presentation/pages/home.dart
@@ -1,13 +1,52 @@
+import 'package:aci_rezeki_shop_mobile/data/models/auth_response_model.dart';
+import 'package:aci_rezeki_shop_mobile/domain/usecases/logout_use_case.dart';
+import 'package:aci_rezeki_shop_mobile/main.dart';
 import 'package:aci_rezeki_shop_mobile/presentation/pages/list_items.dart';
+import 'package:aci_rezeki_shop_mobile/presentation/pages/login.dart';
+import 'package:aci_rezeki_shop_mobile/service_locator.dart';
 import 'package:flutter/material.dart';
 import 'package:aci_rezeki_shop_mobile/presentation/widget/left_drawer.dart';
 import 'package:aci_rezeki_shop_mobile/presentation/widget/menu_card.dart';
 import 'package:aci_rezeki_shop_mobile/presentation/pages/add_item_form.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
+    void logout() async {
+      LogoutUseCase logoutUseCase = getIt<LogoutUseCase>();
+      AuthResponseModel response = await logoutUseCase.execute();
+      if (response.status) {
+        ScaffoldMessenger.of(navigatorKey.currentContext!)
+            .showSnackBar(const SnackBar(
+          content: Text("Logout successfully!"),
+        ));
+        Navigator.pushReplacement(
+          navigatorKey.currentContext!,
+          MaterialPageRoute(builder: (context) => const LoginPage()),
+        );
+      } else {
+        showDialog(
+          context: _scaffoldKey.currentContext!,
+          builder: (context) => AlertDialog(
+            title: const Text('Logout Failed'),
+            content: Text(response.message),
+            actions: [
+              TextButton(
+                child: const Text('OK'),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        );
+      }
+    }
+
     final List<MenuItem> items = [
       MenuItem(
           name: "View Items",
@@ -26,9 +65,13 @@ class HomePage extends StatelessWidget {
                 MaterialPageRoute(builder: (context) => const AddItemForm()));
           }),
       MenuItem(
-          name: "Logout", icon: Icons.logout, color: const Color(0xfffbaf41)),
+          name: "Logout",
+          icon: Icons.logout,
+          color: const Color(0xfffbaf41),
+          onTap: logout),
     ];
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         title: const Text(
           'Aci Rezeki Shop',
--- lib/presentation/widget/left_drawer.dart
+++ lib/presentation/widget/left_drawer.dart
@@ -1,10 +1,46 @@
+import 'package:aci_rezeki_shop_mobile/data/models/auth_response_model.dart';
+import 'package:aci_rezeki_shop_mobile/domain/usecases/logout_use_case.dart';
+import 'package:aci_rezeki_shop_mobile/main.dart';
 import 'package:aci_rezeki_shop_mobile/presentation/pages/list_items.dart';
+import 'package:aci_rezeki_shop_mobile/presentation/pages/login.dart';
+import 'package:aci_rezeki_shop_mobile/service_locator.dart';
 import 'package:flutter/material.dart';
 import 'package:aci_rezeki_shop_mobile/presentation/pages/home.dart';
 import 'package:aci_rezeki_shop_mobile/presentation/pages/add_item_form.dart';
 
 class LeftDrawer extends StatelessWidget {
   const LeftDrawer({super.key});
+
+  void logout() async {
+    LogoutUseCase logoutUseCase = getIt<LogoutUseCase>();
+    AuthResponseModel response = await logoutUseCase.execute();
+    if (response.status) {
+      ScaffoldMessenger.of(navigatorKey.currentContext!)
+          .showSnackBar(const SnackBar(
+        content: Text("Logout successfully!"),
+      ));
+      Navigator.pushReplacement(
+        navigatorKey.currentContext!,
+        MaterialPageRoute(builder: (context) => const LoginPage()),
+      );
+    } else {
+      showDialog(
+        context: navigatorKey.currentContext!,
+        builder: (context) => AlertDialog(
+          title: const Text('Logout Failed'),
+          content: Text(response.message),
+          actions: [
+            TextButton(
+              child: const Text('OK'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -45,7 +81,7 @@ class LeftDrawer extends StatelessWidget {
               Navigator.pushReplacement(
                   context,
                   MaterialPageRoute(
-                    builder: (context) => const HomePage(),
+                    builder: (context) => HomePage(),
                   ));
             },
           ),
@@ -61,6 +97,17 @@ class LeftDrawer extends StatelessWidget {
           ListTile(
             leading: const Icon(Icons.shopping_basket),
             title: const Text('View Items'),
+            onTap: () {
+              // Route menu ke halaman produk
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const ListItemsPage()),
+              );
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.logout),
+            title: const Text('Logout'),
             onTap: () {
               // Route menu ke halaman produk
               Navigator.push(
